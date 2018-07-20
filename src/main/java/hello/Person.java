ebe9059d3ff12f6fcb27e9382204d11f54b293d7
florin horopciuc <florin.horopciuc@arnia.ro>
2018-07-20 17:41:34 +0300

    Initial commit

---
+++ src/main/java/hello/Person.java
@@ -0,0 +1,15 @@
+package hello;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class Person {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+}
